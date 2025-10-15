98f607b7f13cbdc9691f52716a7840837ce311ef
maoudin <mathis.oudin42@gmail.com>
2025-10-15 14:01:33 +0200

    feat: add reassurance section

--- src/features/home/page.tsx
+++ src/features/home/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "@/modules/app/react/hero-section";
+import ReassuranceSection from "@/modules/app/react/reassurance-section";
 
 export default function HomePage() {
     return (
         <React.Fragment>
-            <HeroSection />
+            <HeroSection/>
+            <ReassuranceSection/>
         </React.Fragment>
     )
 }
