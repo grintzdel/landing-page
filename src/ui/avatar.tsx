5cf0a17f57e5fcae1d83a4da8fec1ee9d95199d1
maoudin <mathis.oudin42@gmail.com>
2025-10-15 16:32:20 +0200

    feat: add avatar ui component

---
+++ src/ui/avatar.tsx
@@ -0,0 +1,15 @@
+import {JSX} from "react";
+import Image from "next/image";
+
+type AvatarProps = {
+    src: string;
+    alt: string;
+}
+
+export const Avatar = ({src, alt}: AvatarProps): JSX.Element => {
+    return (
+        <div className="w-15 h-15 rounded-full overflow-hidden bg-white p-2">
+            <Image src={src} alt={alt} width={48} height={48} className="object-cover"/>
+        </div>
+    )
+}
