646ea6a61aa1b7a52bb6d82ca67efbf650cc3122
maoudin <mathis.oudin42@gmail.com>
2025-10-15 16:32:35 +0200

    feat: add testimonials block

---
+++ src/modules/app/react/testimonial.tsx
@@ -0,0 +1,30 @@
+import {JSX} from "react";
+import {Avatar} from "@/ui/avatar";
+import {FaQuoteLeft} from "react-icons/fa";
+
+type TestimonialCardProps = {
+    name: string;
+    since: string;
+    review: string;
+    avatar: string;
+}
+
+export const TestimonialCard = ({name, since, review, avatar}: TestimonialCardProps): JSX.Element => {
+    return (
+        <div className="bg-gray-100 pt-10 pl-10 pr-10 pb-15">
+            <div className="flex flex-col gap-8">
+                <div className="flex flex-row gap-4 items-center">
+                    <Avatar src={avatar} alt={name}/>
+                    <div className="flex flex-col justify-center">
+                        <p className="font-semibold max-w-[110px]">{name}</p>
+                        <p className="text-gray-500 text-[13px] tracking-tight">{since}</p>
+                    </div>
+                    <FaQuoteLeft fill="#3a9843" className="h-9 w-9 ml-0 md:ml-5 lg:ml-10"/>
+                </div>
+                <div>
+                    <p className="text-gray-700 font-light">{review}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/modules/app/react/testimonials-section.tsx
@@ -0,0 +1,51 @@
+import {JSX} from "react";
+import {TestimonialCard} from "@/modules/app/react/testimonial";
+
+const TestimonialsHeader = (): JSX.Element => {
+    return (
+        <div className="flex flex-col justify-center items-center gap-3 mb-12">
+            <p className="text-lg font-medium text-[#3a9843] tracking-tight">Testimonial</p>
+            <h2 className="text-2xl font-bold capitalize lg:text-4xl">What The People Thinks About Us</h2>
+        </div>
+    )
+}
+
+export default function TestimonialsSection() {
+    const testimonials = [
+        {
+            name: "Dwayne Bender",
+            since: "one year with us",
+            review: "Connect with a live host to explore a variety of topics or browse our entire demo library, an experienced developer, we have an option for you.",
+            avatar: "/memoji-3.png"
+        },
+        {
+            name: "Lukan Depina",
+            since: "one year with us",
+            review: "Create custom landing pages with Omega that convert more visitors than any website. With lots of unique blocks, without coding.",
+            avatar: "/memoji-2.png"
+        },
+        {
+            name: "Charles Patterson",
+            since: "one year with us",
+            review: "Create custom landing pages with Omega that convert more visitors than any website. With lots of unique blocks, without coding. .",
+            avatar: "/memoji-3.png"
+        }
+    ];
+
+    return (
+        <section className="py-16 px-8 max-w-full mx-auto">
+            <TestimonialsHeader/>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {testimonials.map((testimonial, index) => (
+                    <TestimonialCard
+                        key={index}
+                        name={testimonial.name}
+                        since={testimonial.since}
+                        review={testimonial.review}
+                        avatar={testimonial.avatar}
+                    />
+                ))}
+            </div>
+        </section>
+    )
+}
